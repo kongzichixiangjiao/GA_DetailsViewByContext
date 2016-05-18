4227a2dadcba352e394416d3191740e813b57af5
kongzichixiangjiao <58895347@qq.com>
2016-05-18 17:42:45 +0800

    001

--- GA_DetailsViewByContext/SBDetailsView/GA_DetailsBackgroundView.swift
+++ GA_DetailsViewByContext/SBDetailsView/GA_DetailsBackgroundView.swift
@@ -30,27 +30,53 @@ class GA_DetailsBackgroundView: UIView {
     
     func createUI() {
         let w = self.frame.size.width
+        //坐标，距离
         let coordinateView = GA_DetailsCoordinateView.createView()
         coordinateView.frame = CGRectMake(0, 200, w, 75)
         coordinateView.setup()
         self.addSubview(coordinateView)
-        
+        //标题，地点，费用
         let dressView = GA_DetailsDressView(frame: CGRectMake(0, CGRectGetMaxY(coordinateView.frame) + spacing, w, 20))
         dressView.setup()
         self.addSubview(dressView)
-        
+        //单独一张图
         let pH = w * w / UIWindow().screen.bounds.height
         let pictureView = UIImageView(frame: CGRectMake(0, CGRectGetMaxY(dressView.frame) + spacing, w, pH))
         let i = UIImage(named: "test.jpg")
         pictureView.image = i
         self.addSubview(pictureView)
-        
+        //内容介绍
         let contentTitleView = GA_DetailsClassView.createView()
         contentTitleView.frame = CGRectMake(0, CGRectGetMaxY(pictureView.frame) + spacing, w, 55)
         contentTitleView.isHideLineView = true
         contentTitleView.classTitleLabel.text = "内容介绍"
         self.addSubview(contentTitleView)
         
+        let imageTextView = UIView()
+        imageTextView.backgroundColor = UIColor.whiteColor()
+        let iTSpacing: CGFloat = 15
+        for i in 0..<2 {
+            if i == 0 {
+                let pictureView = UIImageView(frame: CGRectMake(iTSpacing, iTSpacing, w - iTSpacing * 2, pH))
+                let i = UIImage(named: "test.jpg")
+                pictureView.image = i
+                views.append(pictureView)
+                imageTextView.addSubview(pictureView)
+            } else {
+                let dressLabel = GA_LineHeightLabel(frame: CGRectMake(spacing, CGRectGetMaxY(views[i - 1].frame) + iTSpacing, w - iTSpacing * 2 ,20))
+                dressLabel.myFont = UIFont.systemFontOfSize(12)
+                dressLabel.lineSpace = 10
+                dressLabel.numberOfLines = 0
+                dressLabel.text = "我是地点我是地点我是地点我是地点我是地点我是地点我是地点我是地点我是地点我"
+                views.append(dressLabel)
+                imageTextView.addSubview(dressLabel)
+            }
+        }
+        imageTextView.frame = CGRectMake(0, CGRectGetMaxY(contentTitleView.frame), w, CGRectGetMaxY(views.last!.frame) + iTSpacing)
+        self.addSubview(imageTextView)
+        //图文
+        
+        /*
         let webView = GA_WebView(frame: CGRectMake(0, CGRectGetMaxY(contentTitleView.frame), w, 250))
         webView.backgroundColor = UIColor.redColor()
         webView.tag = 2002
@@ -59,30 +85,38 @@ class GA_DetailsBackgroundView: UIView {
         webView.loadHTMLString(webView.HTMLString(), baseURL: nil)
         webView.myDelegate = self
         self.addSubview(webView)
-        
+         */
+        //用户评论
         let conmentTitleView = GA_DetailsClassView.createView()
-        conmentTitleView.frame = CGRectMake(0, CGRectGetMaxY(webView.frame) + spacing, w, 55)
+        conmentTitleView.frame = CGRectMake(0, CGRectGetMaxY(imageTextView.frame) + spacing, w, 55)
         conmentTitleView.classTitleLabel.text = "用户评论:"
         views.append(conmentTitleView)
         self.addSubview(conmentTitleView)
-
+        //用户评论内容
         let conmentView = GA_DetailsConmentView(frame: CGRectMake(0, CGRectGetMaxY(conmentTitleView.frame), w, 0))
         conmentView.y = CGRectGetMaxY(conmentTitleView.frame)
         views.append(conmentView)
         self.addSubview(conmentView)
-    
+        
+        let conmentView1 = GA_DetailsConmentView(frame: CGRectMake(0, CGRectGetMaxY(conmentView.frame), w, 0))
+        conmentView1.y = CGRectGetMaxY(conmentView.frame)
+        views.append(conmentView1)
+        self.addSubview(conmentView1)
+        //联系方式 更多详情
         let mH: CGFloat = 55
         let moreView = GA_DetailsMoreView.createView()
         moreView.frame = CGRectMake(0, CGRectGetMaxY(views.last!.frame) + spacing, w, mH)
         moreView.tag = 2001
         views.append(moreView)
         self.addSubview(moreView)
-        
+        //结果来自
         let bH: CGFloat = 111
         let beFromeView = GA_DetailsBeFromeView.createView()
         beFromeView.frame = CGRectMake(0, CGRectGetMaxY(moreView.frame) + spacing, w, bH)
         views.append(beFromeView)
         self.addSubview(beFromeView)
+        
+        setupScrollViewContentSize()
     }
     
     func setupScrollViewContentSize() {
@@ -94,6 +128,7 @@ class GA_DetailsBackgroundView: UIView {
 }
 
 extension GA_DetailsBackgroundView: GA_WebViewDelegate {
+
     func webViewDidFinishLoad(contentSizeHeight: CGFloat) {
         let w = self.frame.size.width
         let webView = self.viewWithTag(2002)
